469944bc29362e6595ca2b8e9c222c07405472a3
Ishan Kanaujiya <ishankanaujiya96@gmail.com>
2023-12-06 13:37:47 +0545

    lots of bug detected and  working on it

--- src/main_menu.c
+++ src/main_menu.c
@@ -1,14 +1,13 @@
-#include "../include/mainmenu.h"
-#include "../include/admin.h"
-#include "../include/authentication.h"
-#include "../include/student.h"
+#include "mainMenu.h"
+
 void main_menu()
 {
 	while (1)
 	{
-
+	re_main_menu:
+		system("cls");
 		int choice = 0;
-		printf("\n--------Main Menu-------");
+		printf("--------Main Menu-------");
 		printf("\n1. Admin");
 		printf("\n2. Student");
 		printf("\n3. Exit");
@@ -19,20 +18,21 @@ void main_menu()
 		switch (choice)
 		{
 		case 1:
-			// admin function
-			//  menu();
 			entry();
 			break;
 
 		case 2:
-			// student function
+			view_student_record();
+			fflush(stdin);
+			getchar();
 			break;
 		case 3:
-			exit(0);
+			exit_program(0);
 			break;
 		default:
 			printf("The entered choice is not valid. Please try again....");
 			Sleep(2000);
+			goto re_main_menu;
 			break;
 		}
 	}
